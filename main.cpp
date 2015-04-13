574e3bf0cb9a163b0ab8d52d5d9eee86556d7135
Misak73 <gukasyanm@gmail.com>
2015-04-14 00:18:27 +0300

    zadanie 1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+long double fact(int N)
+{
+    if(N < 0)
+        return 0;
+    if (N == 0)
+        return 1;
+    else
+        return N * fact(N - 1);
+}
+int main()
+{
+    int N;
+    cout << "Vvedite chislo dlya factoriala(po zadaniyu - 3): ";
+    cin >> N;
+    cout << "Factorial dlya chisla " << N << " = " << fact(N) << endl << endl; // fact(N) - функция для вычисления факториала.
+    return 0;
+}
